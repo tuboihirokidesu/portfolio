36d5441a8848b866036b1b9a3e50d6907c4d21e7
tuboihirokidesu <tuboi.hiroki@icloud.com>
2021-03-17 11:56:23 +0900

    app file update

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,63 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Route, RouteComponentProps, Switch } from 'react-router-dom';
 import routes from 'config/routes';
 import GlobalStyle from 'globalStyle';
 import projectRoutes from 'config/projectRoutes';
+import Shop from 'pages/project3/Shop';
+import Cart from 'pages/project3/Cart';
+import {
+  CartContextProvider,
+  cartReducer,
+  initialCartState,
+} from 'components/project3/cart';
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Switch>
-      {routes.map((route) => (
+const App = () => {
+  const [cartState, cartDispatch] = useReducer(cartReducer, initialCartState);
+
+  const cartContextValues = {
+    cartState,
+    cartDispatch,
+  };
+  return (
+    <>
+      <GlobalStyle />
+      <Switch>
+        {routes.map((route) => (
+          <Route
+            key={route.id}
+            path={route.path}
+            exact={route.exact}
+            render={(props: RouteComponentProps<any>) => (
+              <route.component name={route.name} {...props} {...route.props} />
+            )}
+          />
+        ))}
         <Route
-          key={route.id}
-          path={route.path}
-          exact={route.exact}
-          render={(props: RouteComponentProps<any>) => (
-            <route.component name={route.name} {...props} {...route.props} />
+          path="/portfolio/project"
+          render={({ match: { url } }) => (
+            <Switch>
+              {projectRoutes.map((route) => (
+                <Route
+                  key={route.id}
+                  path={`${url}${route.path}`}
+                  exact={route.exact}
+                  component={route.component}
+                />
+              ))}
+              <CartContextProvider value={cartContextValues}>
+                <Route
+                  path={`${url}/shopping-cart`}
+                  exact={false}
+                  component={Shop}
+                />
+                <Route path={`${url}/cart`} exact={false} component={Cart} />
+              </CartContextProvider>
+            </Switch>
           )}
         />
-      ))}
-      <Route
-        path="/portfolio/project"
-        render={({ match: { url } }) => (
-          <Switch>
-            {projectRoutes.map((route) => (
-              <Route
-                key={route.id}
-                path={`${url}${route.path}`}
-                exact={route.exact}
-                component={route.component}
-              />
-            ))}
-          </Switch>
-        )}
-      />
-    </Switch>
-  </>
-);
+      </Switch>
+    </>
+  );
+};
 
 export default App;
